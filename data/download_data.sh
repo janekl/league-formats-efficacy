48b2e5b044aa2ef86d9ccb588fcc3ed175f1302f
Janek <janek.lasek@gmail.com>
2018-08-15 01:08:48 +0200

    Simplifying download script

--- data/download_data.sh
+++ data/download_data.sh
@@ -1,14 +1,14 @@
 for league in D E F I SC SP
 do
-    for i in $(seq 102 116)
+    for i in $(seq 2 16)
     do
         if [ "$league" == "E" ] || [ "$league" == "SC" ]; then 
             div=0
         else
             div=1
         fi
-        m=$(printf %02d $((i % 100)))
-        n=$(printf %02d $(((i+1)%100)))
+        m=$(printf %02d $i)
+        n=$(printf %02d $((i+1)))
         echo http://www.football-data.co.uk/mmz4281/"$m""$n"/"$league""$div".csv
         wget http://www.football-data.co.uk/mmz4281/"$m""$n"/"$league""$div".csv -O "$league"_"$m""$n".csv
         sleep 1s
